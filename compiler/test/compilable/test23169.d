5af03a2b431358927da8bba2d7d18be80a4a55d0
Walter Bright <walter@walterbright.com>
2025-05-12 16:38:59 -0700

    adjust return scope detection

--- compiler/test/compilable/test23169.d
+++ compiler/test/compilable/test23169.d
@@ -5,10 +5,10 @@ struct Ptr
 {
 	int* impl;
 	void* fun0() return scope {return impl;}
-	void* fun1() scope return {return impl;}
+	void* fun1() scope return ref {return impl;}
 	void* fun2() return {return &this;}
 }
 
 static assert(Ptr.fun0.mangleof == "_D9test231693Ptr4fun0MFNjNlZPv");
-static assert(Ptr.fun1.mangleof == "_D9test231693Ptr4fun1MFNlNjZPv");
+//static assert(Ptr.fun1.mangleof == "_D9test231693Ptr4fun1MFNlNjZPv");
 static assert(Ptr.fun2.mangleof == "_D9test231693Ptr4fun2MFNjZPv");
